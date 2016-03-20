72787469ca319ef98a730bd03978939dee2010d1
shanzhao.sam <shanzhao.sam@gmail.com>
2016-03-20 12:37:35 -0400

    add the common

--- synopsys.calculator-0.0.1/src/main/java/synopsys/calculator/common/LoggerImpl.java
+++ synopsys.calculator-0.0.1/src/main/java/synopsys/calculator/common/LoggerImpl.java
@@ -3,6 +3,13 @@ package synopsys.calculator.common;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
+/**
+ *logger implements layer.
+ *In this file, the apach.logger has been used to do the log
+ *We can set up the appender to do the database log. 
+ *
+ *
+ */
 public class LoggerImpl {
 
 	private static Logger logger =  Logger.getRootLogger();
